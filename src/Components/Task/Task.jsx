08a6d22bb8f6587b3382cdd16190a8ac0ef4028d
Artem <derenokartem95@gmail.com>
2022-09-11 19:10:21 +0300

    feat: add Task component

---
+++ src/Components/Task/Task.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import styles from './Task.module.scss';
+
+function Task({time, message}) {
+  return (
+    <div className={styles.task}>
+      <div className={styles.time}>{time}</div>
+      <div className={styles.message}>{message}</div>
+    </div>
+  )
+}
+
+Task.propTypes = {
+  time: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+}
+
+export default Task;
