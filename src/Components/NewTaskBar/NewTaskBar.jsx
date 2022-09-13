970c32bf732b01ae28d1a8721d1adff87cbe780e
Artem <derenokartem95@gmail.com>
2022-09-13 21:00:38 +0300

    refactor: rename NewTaskBar, add validation and redux

--- src/Components/NewTaskBar/NewTaskBar.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './NewTaskBar.module.scss';
-
-function NewTaskBar() {
-  return (
-    <input className={styles.input} type="text" placeholder='Enter a new task' />
-  )
-}
-
-export default NewTaskBar;
---
+++ src/Components/NewTaskForm/NewTaskForm.jsx
@@ -0,0 +1,48 @@
+import { useDispatch } from 'react-redux';
+import { useForm } from "react-hook-form";
+import postTask, { deleteTasks } from '../../redux/asyncActions/tasks';
+import styles from './NewTaskForm.module.scss';
+import FormError from '../FormError/FormError';
+
+
+function NewTaskForm() {
+  const {
+    register,
+    formState: {
+      errors,
+    },
+    handleSubmit,
+  } = useForm();
+
+  const dispatch = useDispatch();
+  
+  const clearAllTasks = () => {
+    dispatch(deleteTasks());
+  }
+  
+  const onSubmit = (data) => {
+    dispatch(postTask({time: data.time, message: data.event, date: new Date().toLocaleDateString()}));
+    
+  }
+  
+  return (
+    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+      <input className={styles.time} type='time' {...register('time', {required: 'Time is required'})} />
+      <input className={styles.text} {...register('event', {required: 'Message is required', minLength: {
+        value: 5,
+        message: 'Minimum 10 characters',
+      }, maxLength: {
+        value: 100,
+        message: 'Мaximum 100 characters',
+      }})} placeholder='Enter a new event' />
+      {errors?.time && <FormError error={errors.time.message} />}
+      {errors?.event && <FormError error={errors.event.message} />}
+      <div>
+        <button type='submit'>ADD</button>
+        <button type='button' onClick={clearAllTasks}>CLEAR ALL</button>
+      </div>
+    </form>
+  )
+}
+
+export default NewTaskForm;
