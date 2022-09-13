c2d74747a2b3ea10a4348b5356a7e61964012a1a
Artem <derenokartem95@gmail.com>
2022-09-13 21:02:01 +0300

    feat: add redux and some styles in ToDoList

--- src/Components/ToDoList/ToDoList.jsx
+++ src/Components/ToDoList/ToDoList.jsx
@@ -1,13 +1,20 @@
+import { useLayoutEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setAllTasks } from '../../redux/asyncActions/tasks';
 import Task from '../Task/Task';
 import styles from './ToDoList.module.scss';
 
 function ToDoList() {
+  const tasks = useSelector(state => state.taskReducer.tasks);
+  const dispatch = useDispatch();
+ 
+  useLayoutEffect(() => {
+    dispatch(setAllTasks());
+  }, [])
+  
   return (
     <div className={styles.list}>
-      <Task time='18:00' message='Lorem ipsum dolor sit amet, consectetur adipiscing elit' />
-      <Task time='18:00' message='Lorem ipsum dolor sit amet, consectetur adipiscing elit' />
-      <Task time='18:00' message='Lorem ipsum dolor sit amet, consectetur adipiscing elit' />
-      <Task time='18:00' message='Lorem ipsum dolor sit amet, consectetur adipiscing elit' />
+      {tasks.map((elem) => <Task key={elem.id} time={elem.time} message={elem.message} />)}
     </div>
   )
 }
