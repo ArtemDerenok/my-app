04c744fbaec0a4bd524aa95a5b9e50d3d2673e3e
Artem <derenokartem95@gmail.com>
2022-09-16 20:39:13 +0300

    feat: add local state

--- src/Components/CitySelector/CitySelector.jsx
+++ src/Components/CitySelector/CitySelector.jsx
@@ -1,15 +1,21 @@
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import styles from './CitySelector.module.scss'
 
 function CitySelector({handleRequest}) {
-  const data = useSelector(state => state.weatherReducer);
-  
+  const [optionValue, setOptionValue] = useState('');
+  const weatherData = useSelector(state => state.weatherReducer);
+    
   return (
-    <select onChange={(e) => handleRequest(e.target.value)}>
-      <option value=''>Select an option</option>
-      {data.openWeatherApi !== null ? 
-      Object.keys(data.openWeatherApi).map((elem) => <option key={nanoid()} value={elem}>{`${elem[0].toUpperCase()}${elem.slice(1)}`}</option>)  
+    <select className={styles.select} value={optionValue} onChange={(e) => {
+      handleRequest(e.target.value, weatherData.currentApi)
+      setOptionValue(e.target.value);
+      }}>
+      <option disabled hidden value=''>Select city</option>
+      {weatherData[weatherData.currentApi] !== null ? 
+      Object.keys(weatherData[weatherData.currentApi]).map((elem) => <option key={nanoid()} value={elem}>{`${elem[0].toUpperCase()}${elem.slice(1)}`}</option>)  
     : null}
     </select>
   )
